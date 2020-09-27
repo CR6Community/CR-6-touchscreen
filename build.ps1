eca41946f6821f8b62f15dede2bc905665dececf
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2020-09-27 16:43:50 +0200

    Add docs about updating the ICL files for the touch screen

--- build.ps1
+++ build.ps1
@@ -1,6 +1,7 @@
 Write-Host "DGUS DWIN firmware build package script v1.0" -ForegroundColor Cyan
 Write-Host ""
-Write-Host "WARNING: Ensure you've clicked 'Generate' in the DWIN editor first" -ForegroundColor Yellow
+Write-Host "WARNING: " -ForegroundColor YellowWrite-Host "1 . Ensure you've clicked 'Generate' in the DWIN editor first" -ForegroundColor Yellow
+Write-Host "2 . If any screens are updated, make sure you've re-generated the ICL file (see README.md)" -ForegroundColor Yellow
 Write-Host "Press any key to confirm!" -ForegroundColor Yellow
 Read-Host | Out-Null
 
