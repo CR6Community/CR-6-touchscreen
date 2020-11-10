d9425fdd93742d4a9375c7f169a4b1a5c23e2be6
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2020-11-10 23:41:04 +0100

    Use Powershell for CR-6 touchscreen releases - for the Mac users
    Windows Powershell being a weird beast again: https://github.com/PowerShell/PowerShell/issues/2140

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -NoProfile -ExecutionPolicy Unrestricted -File %~dp0\%~n0.ps1 -Arguments %*
+pwsh -NoProfile -ExecutionPolicy Unrestricted -File %~dp0\%~n0.ps1 -Arguments %*
 exit /b %ERRORLEVEL%
